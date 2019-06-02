056e2be8c128355de58a7c3dd1ab0296ec942398
Nicolás Careri <nicolascareri@gmail.com>
2019-06-02 19:56:27 -0300

    fixeado password

--- model/Model.php
+++ model/Model.php
@@ -7,7 +7,7 @@
 
       function __construct()
         {
-          $this->db = new PDO('pgsql:host=dbases.exa.unicen.edu.ar; port=6432;dbname=cursada; user=unc_249193; password=Domingo1');
+          $this->db = new PDO('pgsql:host=dbases.exa.unicen.edu.ar; port=6432;dbname=cursada; user=unc_249193; password=');
         }
 
     }
